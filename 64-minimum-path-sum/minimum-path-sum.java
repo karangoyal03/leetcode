8b47da5751f55536e3efe1e236507dc3c4317cd0
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-07-29 00:10:41 -0400

    Time: 1 ms (92.35%) | Memory: 45.7 MB (21.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,94 +1,45 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
+        int m = grid.length-1;
+        int n = grid[0].length-1;
+        // return recursion(grid,m,n);
+        int[][] dp = new int[m+2][n+2];
         for(int[] val : dp){
             Arrays.fill(val,-1);
         }
-        // return recusrion(grid,grid.length-1,grid[0].length-1);
-        // return memoization(grid,grid.length-1,grid[0].length-1,dp);
-        // return tabulation(grid,grid.length,grid[0].length,dp);
-        return spaceOptimization(grid,grid.length,grid[0].length);
+        return memoization(grid,m,n,dp);
     }
 
-    private int recusrion(int[][] grid , int row , int col) {
-        if(row == 0 && col ==0) {
+    private int recursion(int[][] grid , int row , int col) {
+
+        if(row<0  || col<0){
+            return (int)1e9;
+        }
+        if(row == 0 && col ==0){
+            return grid[0][0];
+        }
+        int left = recursion(grid,row,col-1) + grid[row][col];
+        int right = recursion(grid,row-1,col) + grid[row][col];
+
+        return Math.min(left,right);
+    }
+
+    private int memoization(int[][]grid, int row , int col , int[][]dp){
+        if(row< 0 || col < 0){
+            return (int)1e9;
+        }
+
+        if(row ==0 && col == 0){
             return grid[0][0];
         }
 
-        if(row<0  || col<0) {
-            return (int)Math.pow(10, 9);
-        }
-
-        int up =  grid[row][col]+ recusrion(grid,row-1,col);
-        int left =  grid[row][col] + recusrion(grid,row,col-1);
-        return Math.min(up,left);
-    }
-
-    private int memoization(int[][] grid , int row  , int col , int[][]dp) {
-        if(row == 0 && col == 0) {
-            return grid[0][0];
-        }
-
-        if(row<0 || col<0) {
-            return (int)Math.pow(10,9);
-        }
         if(dp[row][col]!=-1){
             return dp[row][col];
         }
 
-        int up = grid[row][col] + memoization(grid,row-1,col,dp);
-        int down = grid[row][col] + memoization(grid,row,col-1,dp);
-        dp[row][col]  = Math.min(up,down);
+        int left = memoization(grid,row,col-1,dp) + grid[row][col];
+        int right = memoization(grid,row-1,col,dp) + grid[row][col];
+        dp[row][col] = Math.min(left,right);
         return dp[row][col];
-    }
-
-    private int tabulation(int[][]grid, int m , int n , int[][] dp) {
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j] = grid[i][j];
-                    continue;
-                }
-                    int up = (int)Math.pow(10,9);
-                    int left = (int)Math.pow(10,9);
-                if(i>0){
-                    up = grid[i][j] + dp[i-1][j];
-                }
-
-                if(j>0) {
-                    left = grid[i][j] + dp[i][j-1];
-                }
-
-                dp[i][j] = Math.min(up,left);
-            }
-        }
-        return dp[m-1][n-1];
-    }
-
-    private int spaceOptimization(int[][]grid, int m , int n) {
-        int[] prev = new int[n];
-        for(int i=0;i<m;i++){
-            int[] temp = new int[n];
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    temp[j] = grid[i][j];
-                    continue;
-                }
-
-                int up = (int)Math.pow(10,9);
-                int left =(int)Math.pow(10,9);
-                if(i>0) {
-                    up = grid[i][j] + prev[j];
-                }
-
-                if(j>0) {
-                    left = grid[i][j] + temp[j-1];
-                }
-
-                temp[j] = Math.min(up,left);
-            }
-            prev = temp;
-        }
-        return prev[n-1];
     }
 }
