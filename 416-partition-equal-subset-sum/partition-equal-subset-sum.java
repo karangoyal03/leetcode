0b1a2d896e8b517cec5e5a5736ba914be6425c72
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-08-18 23:16:51 -0400

    Time: 56 ms (41.96%) | Memory: 55.4 MB (41.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -13,7 +13,8 @@ class Solution {
             for(int[] val : dp) {
                 Arrays.fill(val,-1);
             }
-            return memoization(nums,0,n,totalsum/2,dp);
+            // return memoization(nums,0,n,totalsum/2,dp);
+            return tabulation(nums,totalsum/2,n);
         }
     }
 
@@ -56,5 +57,33 @@ class Solution {
 
         dp[indx][sum] = (pick || notPick) ? 1:0;
         return pick  || notPick;
+    }
+
+    private boolean tabulation(int [] arr , int sum , int n) {
+        boolean[][] dp = new boolean[n][sum+1];
+        for(int i=0;i<n;i++){
+            dp[i][0] = true;
+        }
+
+        if(arr[0]<=sum) {
+            dp[0][arr[0]] = true;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int target =1; target<=sum ;target++) {
+
+
+                boolean notPick = dp[i-1][target];
+                boolean pick = false;
+                if(arr[i] <=target) {
+                    pick = dp[i-1][target-arr[i]];
+                }
+
+                dp[i][target] = (pick || notPick);
+            }
+        }
+
+        return dp[n-1][sum];
+
     }
 }
