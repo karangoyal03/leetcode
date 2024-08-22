8206f55b5d337b8e1d971294b500f2c4bda01c5c
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-08-22 02:15:21 -0400

    Time: 2 ms (15.06%) | Memory: 46 MB (24.35%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -2,11 +2,13 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n =prices.length;
         // return recursion(prices,0,1,n);
-        int[][] dp = new int[n+1][3];
-        for(int[]val:dp){
-            Arrays.fill(val,-1);
-        }
-        return memoization(prices,0,1,n,dp);
+        int[][] dp = new int[n+1][2];
+        // for(int[]val:dp){
+        //     Arrays.fill(val,-1);
+        // }
+        // return memoization(prices,0,1,n,dp);
+        long res = tabulation(prices,n);
+        return (int)res;
     }
 
     private int recursion(int[] prices , int indx , int canBuy , int n) {
@@ -40,5 +42,53 @@ class Solution {
         }
         dp[indx][canBuy] = profit;
         return dp[indx][canBuy];
+    }
+
+    private long tabulation(int[]Arr , int n){
+    //    dp[n][0] = 0;
+    // dp[n][1] = 0;
+
+    // for (int i = n - 1; i >= 0; i--) {
+    //     for (int j = 0; j <= 1; j++) {
+    //         int profit = 0;
+    //         if (j == 1) {
+    //             // If we are allowed to buy
+    //             profit = Math.max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+    //         } else {
+    //             // If we are allowed to sell
+    //             profit = Math.max(prices[i] + dp[i + 1][1], 0 + dp[i + 1][0]);
+    //         }
+    //         dp[i][j] = profit;
+    //     }
+    // }
+
+    // return dp[0][0];
+     long[][] dp = new long[n + 1][2];
+
+        // Initialize the entire dp table with -1
+        for (long[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        // Base condition: If we have no stocks to buy or sell, profit is 0
+        dp[n][0] = dp[n][1] = 0;
+
+        long profit = 0;
+
+        // Iterate through the array in reverse to calculate the maximum profit
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 0) { // We can buy the stock
+                    profit = Math.max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    profit = Math.max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
+                }
+
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][0]; // The maximum profit is stored at dp[0][0]
     }
 } 
