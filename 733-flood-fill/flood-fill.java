047b57bfa3bdff3375fcb7e795029f1ca9756934
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-08-27 17:46:07 -0400

    Time: 2 ms (11.89%) | Memory: 44.9 MB (51.54%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -4,7 +4,8 @@ class Solution {
         int[][] ans = image;
         int[] dx = {0,-1,0,1};
         int[] dy = {-1,0,1,0};
-        dfs(image,sr,sc,dx,dy,color,ans,initialColor);
+        // dfs(image,sr,sc,dx,dy,color,ans,initialColor);
+        bfs(image,sr,sc,dx,dy,color,ans,initialColor);
         return ans;
 
     }
@@ -24,5 +25,29 @@ class Solution {
                 dfs(image,nx,ny,dx,dy,newColor,ans,oldColor);
             }  
         }
+    }
+
+    public void bfs(int[][]image , int sr , int sc , int[] dx , int[] dy , int newColor , int[][] ans , int initialColor){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sr,sc});
+        ans[sr][sc] = newColor;
+        int n = image.length;
+        int m = image[0].length;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for( int i=0;i<size;i++){
+                int[] point = queue.poll();
+                for(int j =0;j<4 ;j++){
+                    int nx = point[0] + dx[j];
+                    int ny = point[1]  + dy[j];
+
+                    if(nx >=0 && ny>=0 && nx<n && ny<m && image[nx][ny] == initialColor && ans[nx][ny] != newColor ) {
+                        queue.offer(new int[]{nx,ny});
+                        ans[nx][ny] = newColor;
+                    }
+                }
+            }
+        }
+
     }
 }
