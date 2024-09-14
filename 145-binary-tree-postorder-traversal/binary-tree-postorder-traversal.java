b0fa32de11a86deb67f20bb81185e3a94d0fb620
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-09-14 17:23:22 -0400

    Time: 0 ms (100.00%) | Memory: 41.6 MB (33.09%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -17,7 +17,9 @@ class Solution {
     List<Integer> ans = new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
             ans = new ArrayList<>();
-            post(root);
+            // post(root);
+            // iterativeMethod(root);
+            iterativeWithOneStack(root);
             return ans;
     }
 
@@ -29,5 +31,55 @@ class Solution {
         post(root.left);
         post(root.right);
         ans.add(root.val);
+    }
+
+    private void iterativeMethod(TreeNode root) {
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+        if(root==null){
+            return;
+        }
+        while(!stack1.isEmpty()){
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+            if(node.left!=null){
+                stack1.push(node.left);
+            }
+
+            if(node.right!=null){
+                stack1.push(node.right);
+            }
+        }
+
+        while(!stack2.isEmpty()){
+            ans.add(stack2.pop().val);
+        }
+    }
+
+    private void iterativeWithOneStack(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while(curr!=null || !stack.isEmpty()){
+
+            if(curr!=null){
+                stack.push(curr);
+                curr = curr.left;
+            }else{
+                TreeNode temp = stack.peek().right;
+
+                if(temp == null){
+                    temp = stack.pop();
+                    ans.add(temp.val);
+                    while(!stack.isEmpty() && temp == stack.peek().right){
+                        temp = stack.pop();
+                        ans.add(temp.val);
+                    }
+                }else{
+                    curr = temp;
+                }
+            }
+        }
     }
 }
