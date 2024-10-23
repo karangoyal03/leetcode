1353283dda506fdbc902608640df08ba6501e522
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-10-23 16:02:11 -0400

    Time: 3 ms (41.65%) | Memory: 46.3 MB (82.60%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,18 +1,27 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-        for(int i=0;i<=n;i++){
-            ans[i] = countOnes(i);
+        // int[] ans = new int[n+1];
+        // for(int i=0;i<=n;i++){
+        //     ans[i] = countOnes(i);
+        // }
+        // return ans;
+        int[] dp = new int[n+1];
+        int offset =1 ;
+        for(int i=1 ;i<=n;i++){
+            if(offset*2 == i){
+                offset = i;
+            }
+            dp[i] = 1 + dp[i- offset];
         }
-        return ans;
+        return dp;
     }
 
-    private int countOnes(int n){
-        int res =0;
-        while(n!=0){
-            n = n & (n-1);
-            res++;
-        }
-        return res;
-    }
+    // private int countOnes(int n){
+    //     int res =0;
+    //     while(n!=0){
+    //         n = n & (n-1);
+    //         res++;
+    //     }
+    //     return res;
+    // }
 }
