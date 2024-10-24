0d4d29e13d6eb858e56dc30b8adc462a9d0bc310
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-10-23 20:24:48 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (60.54%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -9,27 +9,45 @@ class Solution {
         //     }
         // }
 
-        int n = matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j = i+1 ;j<n;j++){
-                int temp = matrix[j][i];
-                matrix[j][i] = matrix[i][j];
-                matrix[i][j] = temp;
+        // int n = matrix.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j = i+1 ;j<n;j++){
+        //         int temp = matrix[j][i];
+        //         matrix[j][i] = matrix[i][j];
+        //         matrix[i][j] = temp;
+        //     }
+        // }
+
+        // for(int[] row : matrix){
+        //     int left =0;
+        //     int right = row.length-1;
+        //     while(left<right) {
+        //         int temp = row[left];
+        //         row[left] = row[right];
+        //         row[right] = temp;
+        //         left++;
+        //         right--;
+        //     }
+        // }
+
+       // approach 3 
+              int left =0;
+        int right = matrix.length-1;
+        while(left < right){
+
+            for(int i=0;i<right-left;i++){
+                int top = left;
+                int bottom = right;
+                int topleft = matrix[top][left+i];
+                matrix[top][left+i] = matrix[bottom-i][left];
+                matrix[bottom-i][left] = matrix[bottom][right-i];
+                matrix[bottom][right-i] = matrix[top+i][right];
+                matrix[top+i][right] = topleft;
             }
+            left++;
+            right--;
         }
 
-        for(int[] row : matrix){
-            int left =0;
-            int right = row.length-1;
-            while(left<right) {
-                int temp = row[left];
-                row[left] = row[right];
-                row[right] = temp;
-                left++;
-                right--;
-            }
-        }
-        
 
 
     }
