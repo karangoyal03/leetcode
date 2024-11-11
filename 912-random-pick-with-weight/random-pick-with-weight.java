a92d15f28dc0acec8d74c3e7d077180c7d2af75c
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-11-11 10:54:14 -0500

    Time: 26 ms (42.21%) | Memory: 49.9 MB (37.70%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.java
+++ 912-random-pick-with-weight/random-pick-with-weight.java
@@ -4,40 +4,31 @@ class Solution {
 
   public Solution(int[] w) {
     pfx = new int[w.length];
-
     pfx[0] = w[0];
-
-    for (int i = 1; i < w.length; i++) {
-      pfx[i] = pfx[i - 1] + w[i];
+    for(int i=1;i<w.length;i++){
+        pfx[i] =pfx[i-1]+w[i];
     }
-
-    sum = pfx[pfx.length - 1];
+    sum = pfx[pfx.length-1];
   }
 
   public int pickIndex() {
-      Random random = new Random();
-      int randomNumber = random.nextInt( 0 , sum);
-
-      return binarySearch(randomNumber);
+        Random random = new Random();
+        int randomIndex = random.nextInt(0,sum);
+        return binarySearch(randomIndex);
     }
 
     private int binarySearch(int target){
-      int l = 0;
-      int r = pfx.length - 1;
-     // int res = -1;
-
-      while(l <= r){
-        int m = l + (r-l)/2;
-
-        if(pfx[m] > target ){
-       //   res = m;
-          r = m - 1;
-        } else{
-          l = m + 1;
+        int left =0;
+        int right = pfx.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(pfx[mid] <=target){
+                left =mid+1;
+            }else{
+                right =mid-1;
+            }
         }
-      }
-
-      return l;
+        return left;
     }
 }
 
