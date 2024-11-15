0ce8ac02f5017290ecc55ecae7734ac107e51143
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2024-11-15 17:43:57 -0500

    Time: 3 ms (43.31%) | Memory: 41.6 MB (55.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,39 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            // agar opening h toh insert mtlb push
-            if(ch == '(' || ch == '[' || ch == '{'){
-                stack.push(ch);
-            }else if(ch == ')'){
-                if(!stack.isEmpty() && stack.peek() == '('){
-                    stack.pop();
-                }else{
-                    return false;
-                }
-            }else if(ch =='}'){
-                if(!stack.isEmpty() && stack.peek() == '{'){
-                    stack.pop();
-                }else{
-                    return false;
-                }
-            }else if(ch == ']'){
-                if(!stack.isEmpty() && stack.peek() == '['){
-                    stack.pop();
-                }else{
-                    return false;
-                }
-            }
-            // agr closing h toh delete pop from stack
-            // ismein agr closing wala h toh array ka peek element uska opposite wala 
-            // hona chahiye tabhi woh pop kr skta h
-
-        }
-        if(stack.isEmpty()){
-                return true;
+      Stack<Character> stack = new Stack<>();
+      for(int i=0;i<s.length();i++){
+       if(s.charAt(i)=='('|| s.charAt(i)=='{' || s.charAt(i)=='['){
+        stack.push(s.charAt(i));
+       }else{
+            if(!stack.isEmpty() && stack.peek()== '(' && s.charAt(i) ==')'){
+                stack.pop();
+            }else if(!stack.isEmpty() && stack.peek()=='{' && s.charAt(i) =='}'){
+                stack.pop();
+            }else if(!stack.isEmpty() && stack.peek() =='[' && s.charAt(i) ==']'){
+                stack.pop();
             }else{
                 return false;
-            }  
+            }
+       }
+      }
+      return stack.isEmpty();  
     }
 }
