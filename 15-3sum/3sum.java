cb3db35496ddef1fa74c8e7fd4694d12eb008e5e
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2025-06-05 23:34:09 -0400

    Time: 294 ms (23.02%) | Memory: 53 MB (12.30%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,22 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-      Arrays.sort(nums);
-      List<List<Integer>> ans = new ArrayList<>();
-      int n = nums.length;
-      for(int i=0;i<n;i++){
-        if(i>0 && nums[i] == nums[i-1]){
-            continue;
-        }
-        int j = i+1;
-        int k = n-1;
+        Map<Integer, Boolean> map = new HashMap<>();
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                int rem = 0 - (nums[i] + nums[j]);
+                 if (map.containsKey(rem)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], rem);
+                    Collections.sort(triplet); // sort to avoid duplicates
+                    set.add(triplet);
+                }
+                map.put(nums[j], true);
 
-        while(j<k){
-            int sum = nums[i] + nums[j] + nums[k];
-            if(sum<0){
-                j++;
-            }else if (sum>0){
-                k--;
-            }else{
-                List<Integer> tempAns = new ArrayList<>();
-                tempAns.add(nums[i]);
-                tempAns.add(nums[j]);
-                tempAns.add(nums[k]);
-                ans.add(tempAns);
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1]){j++;}
-                while(j<k && nums[k]==nums[k+1]){k--;}
             }
+            map.clear();
         }
-      }
-      return ans;
-
+        return new ArrayList<>(set);
     }
 }
