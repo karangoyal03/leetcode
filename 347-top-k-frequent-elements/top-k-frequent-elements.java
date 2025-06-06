0b0be0b5fd963b1d6ebc40fb7ba7c96ec6d5f18b
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2025-06-06 12:49:41 -0400

    Time: 13 ms (76.79%) | Memory: 49.2 MB (18.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,29 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-    // calculate the frequenecy of every integer
-
-    Map<Integer,Integer> map = new HashMap<>();
-    for(int num : nums){
-        map.put(num , map.getOrDefault(num,0)+1);
-    }
-
-    PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->a[0]-b[0]);
-    for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-        minHeap.offer(new int[]{entry.getValue(),entry.getKey()});
-
-        if(minHeap.size()>k){
-            minHeap.poll();
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num , map.getOrDefault(num,0)+1);
         }
 
-    }
-
-    int[] elements = new int[k];
-    for(int i =0;i<k;i++)
-        {
-          elements[i] = minHeap.poll()[1];  
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            queue.offer(new int[]{entry.getValue(),entry.getKey()});
+            if(queue.size()>k){
+                queue.poll();
+            }
         }
 
-        return elements;
-
+        int[] result = new int[k];
+        for(int i=0;i<k;i++){
+            result[i] = queue.poll()[1];
+        }
+        return result;
     }
 }
