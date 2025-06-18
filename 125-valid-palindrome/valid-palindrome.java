6e8d1106d93fd27569b3a71f5673619f98eeb629
KARAN GOYAL <49104496+karangoyal03@users.noreply.github.com>
2025-06-17 21:48:28 -0400

    Time: 13 ms (41.10%) | Memory: 44.9 MB (39.28%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,13 +4,12 @@ class Solution {
         int start =0;
         int end = s.length()-1;
         while(start<end){
-            if(s.charAt(start) != s.charAt(end)){
+            if(s.charAt(start)!=s.charAt(end)){
                 return false;
             }
             start++;
             end--;
         }
-
         return true;
     }
 }
